03b5b66f17ff61dd93ebfeb2a9d50d3e05e6a237
kenn <Kenn.Li@partner.bmwgroup.com>
2022-05-09 14:45:52 +0800

    2222

--- app/src/main/java/com/itcast8/mytest/MainActivity.java
+++ app/src/main/java/com/itcast8/mytest/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        master
-      333
+  2222
     }
 }
